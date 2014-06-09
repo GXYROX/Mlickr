6521aa5cb5a6f595ef33d1985ea254a3074e8808
TEENSTIAN <teenstian@gmail.com>
2014-06-09 16:53:31 +0800

    Mlickr

---
+++ Mlickr/src/com/teenstian/mlickr/MainActivity.java
@@ -0,0 +1,78 @@
+package com.teenstian.mlickr;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.igexin.sdk.PushManager;
+import com.umeng.message.PushAgent;
+import com.umeng.message.UmengRegistrar;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+		
+		//umeng
+		PushAgent mPushAgent = PushAgent.getInstance(MainActivity.this);
+	    mPushAgent.enable();
+	    
+	    PushAgent.getInstance(MainActivity.this).onAppStart();
+	    
+	    String device_token = UmengRegistrar.getRegistrationId(MainActivity.this);
+	    System.out.println("device_token---->" + device_token);
+	    
+	    //igetui (gexin)
+	    PushManager.getInstance().initialize(this.getApplicationContext());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
